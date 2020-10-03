efde870c11bb855dd7823f681497eeee0f04895f
AlisinaAyam <72173271+AlisinaAyam@users.noreply.github.com>
2020-10-02 20:55:36 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var fixedrect,movingrect
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  fixedrect=createSprite(600,500,50,80);
+  movingrect=createSprite(600,500,80,50);
+  movingrect.debug=true;
+  movingrect.velocityX=-3;
+  fixedrect.debug=true;
+  fixedrect.velocityX=3;
+  fixedrect.shapeColor="red";
+  movingrect.shapeColor="green";
+}
+
+function draw() {
+  background(255,25,255); 
+  //movingrect.x=mouseX;
+  //movingrect.y=mouseY; 
+  if(movingrect.x-fixedrect.x < fixedrect.width/2+movingrect.width/2
+    && fixedrect.x-movingrect.x < fixedrect.width/2+movingrect.width/2){
+      fixedrect.velocityX=fixedrect.velocityX*(-1);
+      movingrect.velocityX=movingrect.velocityX*(-1);
+    }
+
+     if( movingrect.y-fixedrect.y< fixedrect.height/2+movingrect.height/2
+      && fixedrect.y-movingrect.y < fixedrect.height/2+movingrect.height/2){
+        fixedrect.velocityY=fixedrect.velocityY*(-1);
+        movingrect.velocityY=movingrect.velocityY*(-1);
+  }
+  
+  drawSprites();
+}
